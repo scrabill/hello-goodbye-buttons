7cd0f09fc46de29cab36820a040d6b8767a3a4d3
scrabill <shannon.crabill@gmail.com>
2019-08-04 12:50:38 -0400

    Adds constants

--- js/main.js
+++ js/main.js
@@ -1,30 +1,5 @@
-// Add event lsitener
-// function buttonIsClicked() {
-//   const helloBtn = document.getElementByID("hello")
-//
-//   helloBtn.addEventListener("click", createRows)
-//   console.log("Clicking")
-// }
+// Set constants for later user
 
-const helloBtn = document.getElementById("hello")
+const helloBtn = document.getElementById("hello");
 const goodbyeBtn = document.getElementById("goodbye");
-// const buttons = document.getElementsByTagName("button");
-// const buttons = document.getElementsByClassName("btn")
-
-helloBtn.addEventListener("click", createRows)
-goodbyeBtn.addEventListener("click", createRows)
-
-// goodbyeBtn.addEventListener("click", createRows)
-
-// document.getElementById("hello").addEventListener("click", test)
-
-function createRows() {
-  let id = event.target.id;
-  console.log(id);
-  if (id == "hello") {
-    document.getElementById("show-rows").innerText = "hello"
-  } else if (id == "goodbye") {
-    document.getElementById("show-rows").innerText = "goodbye"
-  }
-
-}
+const row = document.getElementById("row");
