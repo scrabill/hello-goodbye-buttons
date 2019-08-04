34bd54ec62c8059b9dffa994599f1c7a09d1e5f6
scrabill <shannon.crabill@gmail.com>
2019-08-04 12:52:07 -0400

    Adds event listeners

--- js/main.js
+++ js/main.js
@@ -3,3 +3,8 @@
 const helloBtn = document.getElementById("hello");
 const goodbyeBtn = document.getElementById("goodbye");
 const row = document.getElementById("row");
+
+// Event listers
+
+helloBtn.addEventListener("click", createRows)
+goodbyeBtn.addEventListener("click", createRows)
