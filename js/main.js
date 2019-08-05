86ba147791d2367a129aeed9fe91f887fd9cec68
scrabill <shannon.crabill@gmail.com>
2019-08-04 19:23:54 -0700

    Refactors addEventListener to one use on the buttons array

--- js/main.js
+++ js/main.js
@@ -1,23 +1,20 @@
 // Set constants for later user
 
-const helloBtn = document.getElementById("hello");
-const goodbyeBtn = document.getElementById("goodbye");
 const row = document.getElementById("row");
+const allButtons = document.querySelectorAll("button");
 
-// Event listers
+// Get all button elements, spread them into an array, then add an addEventListener to each button item from the array
 
-helloBtn.addEventListener("click", createRows)
-goodbyeBtn.addEventListener("click", createRows)
+[...document.querySelectorAll("button")].forEach(function(item) {
+    item.addEventListener("click", createRows)
+  });
 
-// Function to create a new row when either button is buttonIsClicked
+// Functio to create a new row when either button is clicked
 
 function createRows() {
 
   let id = event.target.id;
-  console.log(id);
-
   const div = document.createElement("div");
-  console.log("A div has been created");
 
   row.append(div);
 
